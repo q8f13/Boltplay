7c9361e2e3aca62b5736bb79098350b579b6c4f3
q8f13 <dustfall@gmail.com>
2019-03-04 19:09:07 +0800

    remove samples and bolt 102 codes

---
+++ Assets/HelloBolt/HelloBoltMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UdpKit;
+using System;
+
+public class HelloBoltMenu : Bolt.GlobalEventListener
+{
+	void OnGUI()
+	{
+		GUILayout.BeginArea(new Rect(10, 10, Screen.width - 20, Screen.height - 20));
+
+		if (GUILayout.Button("Start Server", GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true)))
+		{
+			// START SERVER
+			BoltLauncher.StartServer();
+		}
+
+		if (GUILayout.Button("Start Client", GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true)))
+		{
+			// START CLIENT
+			BoltLauncher.StartClient();
+		}
+
+		GUILayout.EndArea();
+	}
+
+	public override void BoltStartDone()
+	{
+		if (BoltNetwork.isServer)
+		{
+			string matchName = Guid.NewGuid().ToString();
+
+			BoltNetwork.SetServerInfo(matchName, null);
+			BoltNetwork.LoadScene("helloBolt");
+		}
+	}
+
+	public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
+	{
+		Debug.LogFormat("Session list updated: {0} total sessions", sessionList.Count);
+
+		foreach (var session in sessionList)
+		{
+			UdpSession photonSession = session.Value as UdpSession;
+
+			if (photonSession.Source == UdpSessionSource.Photon)
+			{
+				BoltNetwork.Connect(photonSession);
+			}
+		}
+	}
+}
---
+++ Assets/HelloBolt/NetworkCallbacks.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkCallbacks : Bolt.GlobalEventListener
+{
+    public override void SceneLoadLocalDone(string scene)
+    {
+        Vector3 spawn_pos = new Vector3(Random.Range(-8,8), 0, Random.Range(-8,8));
+
+        BoltNetwork.Instantiate(BoltPrefabs.Cube, spawn_pos, Quaternion.identity);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
